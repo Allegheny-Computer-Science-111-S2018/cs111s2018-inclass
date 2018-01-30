e66d7c3b50c6ea3fa1b17f82377b476e978cecde
Janyl Jumadinova <jjumadinova@allegheny.edu>
2018-01-30 12:49:13 -0500

    Added a program template for Jan 31

---
+++ weeks2-3/AgeStats.java
@@ -0,0 +1,19 @@
+/** This class performs basic age conversions.
+ * The program utilizes the Scanner class to
+ * obtain the input from the user.
+ *
+ * 31 January, 2018
+ *
+ * @author Janyl Jumadinova
+ */
+public class AgeStats
+{
+  //----------------------------
+ 	// main method: program execution begins here
+ 	//----------------------------
+  public static void main(String[] args)
+ 	{
+ 	  // Label output with name and date:
+    System.out.println("Janyl Jumadinova \n " + new Date() + "\n");
+ 	}
+}
