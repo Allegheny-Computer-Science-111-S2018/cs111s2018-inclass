f832317affa1660f35ecb647179d812efda381d3
Janyl Jumadinova <jjumadinova@allegheny.edu>
2018-02-21 12:22:55 -0500

    Class exercise feb 21

--- weeks5-6/Payments.java
+++ weeks5-6/Payments.java
@@ -15,6 +15,16 @@ public class Payments
 		// obtain user's input
 		System.out.println("Enter a loan amount");
 		loanAmount = scan.nextDouble();
+		System.out.println("Enter a rate");
+		rate = scan.nextDouble();
+		System.out.println("Enter loan term");
+		loanTerm = scan.nextInt();
+		
+		double term = Math.pow(1 + rate, loanTerm);
+		System.out.println("Term is: " + term); 
+		
+		double payment = loanAmount * rate * (term / (term - 1));
+		System.out.println("Payment: "+ payment);
 		
 	}
 	
