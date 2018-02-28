0bf5e8d9c7834df634d42cb74b8e3ec46c2fbda9
Janyl Jumadinova <jjumadinova@allegheny.edu>
2018-02-28 11:24:01 -0500

    Updated class exercise

--- week7/IsVowel.java
+++ week7/IsVowel.java
@@ -8,29 +8,24 @@ public class IsVowel
     System.out.println("Enter a character");
     char character;
     character = input.next().charAt(0);
-    if(character == 'a')
-    {
-      System.out.println("Character " + character + " is a vowel");
-    }
-    else if(character == 'o')
-    {
-      System.out.println("Character " + character + " is a vowel");
-    }
-    else if(character == 'e')
-    {
-      System.out.println("Character " + character + " is a vowel");
-    }
-    else if(character == 'i')
-    {
-      System.out.println("Character " + character + " is a vowel");
-    }
-    else if(character == 'u')
+    if(character == 'a' || character == 'o'
+      || character == 'e' || character == 'i'
+      || character == 'u')
     {
       System.out.println("Character " + character + " is a vowel");
     }
     else
     {
       System.out.println("Character " + character + " is not a vowel");
+    }
+
+    if( 2 > 1 && 2 < 3)
+    {
+      System.out.println("True");
+    }
+    else
+    {
+      System.out.println("False");
     }
   }
 }
