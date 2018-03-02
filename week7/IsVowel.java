c39b51ab3db73fcb5caf256951dc40a60ca5161f
Janyl Jumadinova <jjumadinova@allegheny.edu>
2018-03-02 09:29:23 -0500

    Adopted Google style for if statements in IsVowel program

--- week7/IsVowel.java
+++ week7/IsVowel.java
@@ -10,21 +10,15 @@ public class IsVowel
     character = input.next().charAt(0);
     if(character == 'a' || character == 'o'
       || character == 'e' || character == 'i'
-      || character == 'u')
-    {
+      || character == 'u') {
       System.out.println("Character " + character + " is a vowel");
-    }
-    else
-    {
+    } else {
       System.out.println("Character " + character + " is not a vowel");
     }
 
-    if( 2 > 1 && 2 < 3)
-    {
+    if( 2 > 1 && 2 < 3) {
       System.out.println("True");
-    }
-    else
-    {
+    } else {
       System.out.println("False");
     }
   }
