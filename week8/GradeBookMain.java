1bd39f21f8a506d10caee05797cf21d63bcb6b03
Janyl Jumadinova <jjumadinova@allegheny.edu>
2018-03-05 08:41:31 -0500

    Gradebook example

---
+++ week8/GradeBookMain.java
@@ -0,0 +1,11 @@
+/** Create a class GradeBookMain to instantiate GradeBook class
+*/
+
+public class GradeBookMain {
+  /** main method where execution begins.
+  */
+  public static void main (String args[]) {
+    GradeBook myGradeBook  = new GradeBook ();
+      myGradeBook.displayMessage();
+  }
+}
