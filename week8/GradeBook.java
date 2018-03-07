a2b9c4d3f4b906e390d6e654486f954bedba2272
Janyl Jumadinova <jjumadinova@allegheny.edu>
2018-03-07 08:59:22 -0500

    class program and exam handout

--- week8/GradeBook.java
+++ week8/GradeBook.java
@@ -2,9 +2,37 @@
 */
 
 public class GradeBook {
-    /** method to display a welcome message.
-    */
-    public void displayMessage() {
-        System.out.println("Welcome to the Grade Book!");
-    }
+
+  // instance variable
+  private String courseName = "Music 101";
+  private String courseTag = "QR";
+
+  /** Method to change the value of the courseName.
+  */
+  public void setCourseName (String name) {
+    courseName = name;
+  }
+
+  /** Method to return the value of the courseName.
+  */
+  public String getCourseName() {
+    return courseName;
+  }
+
+  /** Method to change the value of the courseName.
+  */
+  public void setCourseTag (String tag) {
+    courseTag = tag;
+  }
+
+  /** Method to return the value of the courseName.
+  */
+  public String getCourseTag() {
+    return courseTag;
+  }
+  /** method to display a welcome message.
+  */
+  public void displayMessage() {
+    System.out.println("Welcome to the Grade Book for " + getCourseName()+" tagged "+ getCourseTag());
+  }
 }
--- week8/GradeBookMain.java
+++ week8/GradeBookMain.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /** Create a class GradeBookMain to instantiate GradeBook class
 */
 
@@ -5,7 +7,28 @@ public class GradeBookMain {
   /** main method where execution begins.
   */
   public static void main (String args[]) {
+    Scanner scanner = new Scanner(System.in);
+
+    //course name
+    System.out.println("Enter a course name: ");
+    String name = scanner.nextLine();
+
+    System.out.println();
+
     GradeBook myGradeBook  = new GradeBook ();
-      myGradeBook.displayMessage();
+    System.out.println("Original name: " + myGradeBook.getCourseName());
+    myGradeBook.setCourseName(name);
+    myGradeBook.displayMessage();
+    System.out.println();
+
+    // course tag
+    System.out.println("Enter a course tag: ");
+    String tag = scanner.next();
+
+    System.out.println();
+
+    System.out.println("Original tag: " + myGradeBook.getCourseTag());
+    myGradeBook.setCourseTag(tag);
+    myGradeBook.displayMessage();
   }
 }
